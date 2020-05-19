0ed09ebe2db74291dc7f56e53c5b9307f347f3ac
zhangyong <758922096@qq.com>
2020-05-19 09:21:19 +0800

    登录表单校验和重置表单

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,16 +7,22 @@
         <img src="@/assets/logo.png" alt="" />
       </div>
       <!-- 表单区域 -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input
             v-model="loginForm.username"
             prefix-icon="iconfont icon-user"
           ></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             v-model="loginForm.password"
             prefix-icon="iconfont icon-3702mima"
@@ -26,7 +32,7 @@
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -41,10 +47,28 @@ export default {
       loginForm: {
         username: 'admin',
         password: '123456'
+      },
+      loginFormRules: {
+        // 验证用户名
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
       }
     }
   },
-  methods: {},
+  methods: {
+    // 重置表单
+    resetLoginForm() {
+      // console.log(this)
+      this.$refs['loginFormRef'].resetFields()
+    }
+  },
   mounted() {},
   components: {}
 }
