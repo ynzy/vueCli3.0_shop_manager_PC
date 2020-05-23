207815e29d28ffde52678f31c14ca9738f41e793
zhangyong <758922096@qq.com>
2020-05-23 09:44:28 +0800

    面包屑导航

---
+++ src/components/crumbs/index.vue
@@ -0,0 +1,30 @@
+<!-- 面包屑导航 -->
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item v-for="crumb in crumbList" :key="crumb.id">{{
+        crumb.authName
+      }}</el-breadcrumb-item>
+      <!-- <el-breadcrumb-item>活动列表</el-breadcrumb-item> -->
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'crumbs',
+  props: {
+    crumbList: Array
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  components: {}
+}
+</script>
+<style lang="less" scoped>
+/* @import url(); 引入css类 */
+</style>
