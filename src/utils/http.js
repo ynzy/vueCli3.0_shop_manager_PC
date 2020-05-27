e566434973e3893839f5b6256496d8856fece9ce
zhangyong <758922096@qq.com>
2020-05-27 09:11:40 +0800

    feat(model:http): 判断token失效的操作

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { storage, sessionStorage } from '@/utils/storage'
+import { Message } from 'element-ui'
+import router from '../router/index'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
@@ -19,22 +21,35 @@ service.interceptors.request.use(
 )
 //http 200 状态码下的异常map
 const erorrMap = {
-  CMN00000: '成功',
-  CMN00001: '输入参数为空',
-  CMN00002: '输入参数校验失败'
+  200: '请求成功',
+  201: '创建成功',
+  204: '删除成功',
+  400: '请求的地址不存在或者包含不支持的参数',
+  401: '未授权',
+  403: '被禁止访问',
+  404: '请求的资源不存在',
+  422: '[POST/PUT/PATCH] 当创建一个对象时，发生一个验证错误',
+  500: '内部错误'
 }
 // http响应拦截器
 service.interceptors.response.use(
   res => {
     //可以根据后端的系统而相应的做调整
     let status = res.data.meta.status
-    var statusArr = [200, 201]
+    var statusArr = [200, 201, 204]
     // 如果不包含此状态就是失败
-    if (!statusArr.includes(status)) {
-      return Promise.reject(res.data)
+    if (statusArr.includes(status)) {
+      return Promise.resolve(res.data)
     } else {
       if (erorrMap[status]) {
         //erorrMap[code]
+        if (status == 400) {
+          // token失效
+          Message.error(res.data.meta.msg)
+          sessionStorage.remove('token')
+          router.replace('/login')
+        }
+        return Promise.reject(res.data)
       } else {
         return Promise.resolve(res.data)
       }
@@ -49,6 +64,7 @@ service.interceptors.response.use(
     } else if (error.response) {
       if (error.response.status === 400) {
         //请求参数有问题
+        Message.error(error)
       } else if (error.response.status === 404) {
         //未找到资源
       } else if (error.response.status === 401) {
